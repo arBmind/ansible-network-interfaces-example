ede9955534ec56da32e0d0545861ea6b09283fcd
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2016-04-01 11:48:11 +0200

    moved to ansible 2.0 and usage of more features

--- ansible/install.sh
+++ ansible/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ANSIBLE_GIT_REPO=git://github.com/dresden-weekly/ansible.git
-ANSIBLE_VERSION=${ANSIBLE_VERSION:=1.9.2-clean}
+#ANSIBLE_VERSION=${ANSIBLE_VERSION:=1.9.2-clean}
+ANSIBLE_VERSION=${ANSIBLE_VERSION:=2.0.1-clean}
 
 source $PROJECT_FOLDER/$VAGRANT_ANSIBLE_REMOTE/ansible/git-install.sh
